559e037b13a26c2e77751ae760c852423d9f9062
r4yX <108579252+r4yX@users.noreply.github.com>
2023-03-03 04:22:54 -0300

    Update totrixxel.sh

--- totrixxel.sh
+++ totrixxel.sh
@@ -2,12 +2,82 @@
 #Variables
 root_verify=$(whoami)
 usuario=$(users)
+text=$(ifconfig | grep broadcast); arr=(${text//" "/ }); ip=${arr[1]}
+
+#Colours
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+PINK="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+NORMAL="\033[0;39m"
+
+#Cierre inesperado
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo "Seguro que quieres salir?[Y/n] >"; read closeV
+    
+    if [ ${closeV,,} != "n" ]
+    then
+        echo -e "$RED[!] Cerrando Apache Server."
+        service apache2 stop
+        if [ $(ps aux | grep ngrok | wc -l) -gt ]
+        then
+            ps aux | grep ngrok | head -n 1 > data ; pid=$(awk '{print $2}' data)
+            rm -rf data
+            kill $pid
+        fi
+        echo -e "$GREEN[+] Apache Server Closed."
+        echo -e "[+] Ngrok Server Closed."
+        echo -e "[+] Totrixxel Cerrado exitosamente...$NORMAL"
+        sleep 1.8
+        clear
+        exit 0
+    fi 
+}
 
 #Programa
+function banner() {
+    echo -e "$YELLOW┏━━━━━━━━━━┓       ┏━━━━━━━━━━┓               ┏━━┓    ┏━━┓                 " 
+    echo -e "$YELLOW┃          ┃       ┃          ┃$NORMAL         ┏━┓$YELLOW   ┗┓ ┗┓  ┏┛ ┏┛$NORMAL              ┏━┓"
+    echo -e "$YELLOW┗━━━┓  ┏━━━┛       ┗━━━┓  ┏━━━┛$NORMAL         ┗━┛$YELLOW    ┗┓ ┗┓┏┛ ┏┛$NORMAL    ┏━━━━━━┓   ┃ ┃"
+    echo -e "$YELLOW    ┃  ┃$NORMAL   ┏━━━━━━━┓$YELLOW   ┃  ┃$NORMAL   ┏━━━━━┓   ┏━┓$YELLOW     ┗┓ ┗┛ ┏┛$NORMAL     ┃ ┏━━┓ ┃   ┃ ┃"
+    echo -e "$YELLOW    ┃  ┃$NORMAL   ┃ ┏━━━┓ ┃$YELLOW   ┃  ┃$NORMAL   ┃ ┏━━━┛   ┃ ┃$YELLOW     ┏┛ ┏┓ ┗┓$NORMAL     ┃ ┃━━┛ ┃   ┃ ┃"
+    echo -e "$YELLOW    ┃  ┃$NORMAL   ┃ ┃   ┃ ┃$YELLOW   ┃  ┃$NORMAL   ┃ ┃       ┃ ┃$YELLOW    ┏┛ ┏┛┗┓ ┗┓$NORMAL    ┃ ┃━━━━┛   ┃ ┃" 
+    echo -e "$YELLOW    ┃  ┃$NORMAL   ┃ ┗━━━┛ ┃$YELLOW   ┃  ┃$NORMAL   ┃ ┃       ┃ ┃$YELLOW   ┏┛ ┏┛  ┗┓ ┗┓$NORMAL   ┃ ┗━━━━┓   ┃ ┃"
+    echo -e "$YELLOW    ┗━━┛$NORMAL   ┗━━━━━━━┛$YELLOW   ┗━━┛$NORMAL   ┗━┛       ┗━┛$YELLOW   ┗━━┛    ┗━━┛$NORMAL   ┗━━━━━━┛   ┗━┛"
+}
+
 function init_program() {
-    echo -e "Bienvenido a Totrixxel..."
-    echo -e "Que quieres hacer?"
-    echo -e "[1] Montar pagina local      [2] Descargar prefabs (git)\n[3] Iniciar pagina local     [4] NetCat"; read qaction
+    banner
+    if [ $(type -P ngrok) != "" ]
+    then
+        ngrok tcp 80 --log reg > /dev/null &
+        sleep 1.6
+        url=$(cat reg | tr "ul" "\n" | tail -1 | cut -c8-99)
+        ps aux | grep ngrok | head -n 1 > data ; pid=$(awk '{print $2}' data)
+        rm -rf data reg
+        ngrokUrl=$(echo $url | tr ":" "\n" | head -n 1)
+        ngrokPort=$(echo $url | tr ":" "\n" | tail -1)
+    else
+        clear
+        echo "[*] Ngrok Server is not configured. https://ngrok.com"
+    fi
+    if [ $(ps aux | grep www-data | wc -l) -lt 2 ]
+    then
+        service apache2 start
+    fi
+    echo -e "[*] Servidor Apache iniciado en $CYAN$ip$GREEN:80$NORMAL"
+    if [ $(ps aux | grep ngrok | wc -l) -gt 1 ]; then
+        echo -e "[*] Servidor Ngrok iniciado en $CYAN$ngrokUrl$GREEN:$ngrokPort$NORMAL"
+    fi
+    
+    echo -e "\nQue quieres hacer?"
+    echo -e "[1] Montar pagina local      [2] Descargar prefabs (git)\n[3] Iniciar pagina local"; read qaction
     if [ $qaction == "1" ]
     then
         echo -e "Tienes una pagina?[y/n] >> "; read qpagexist
@@ -47,12 +117,14 @@ function init_program() {
         else
             echo -e "[-] Err: Opcion no existe."
         fi
-        service apache2 start
+        service apache2 restart
         echo -e "[*] Totrixxel está ejecutando la página en vivo..."
         echo -e "Presiona 'Enter' para dejar de ejecutar el servidor..."; read
         echo -e "[*] Totrixxel está cerrando el servidor..."
         service apache2 stop
-        echo -e "[*] El servidor xTx se ha cerrado correctamente."
+        echo -e "[*] El servidor Apache se ha cerrado correctamente."
+        kill $pid
+        echo -e "[*] El servidor Ngrok se ha cerrado correctamente."
     elif [ $qaction == "2" ]
     then
         echo -e "Creador del repo"; read repouser
@@ -63,24 +135,13 @@ function init_program() {
     	git clone https://www.github.com/$repouser/$reponame
     elif [ $qaction == "3" ]
     then
-        service apache2 start
         echo -e "[*] Totrixxel está ejecutando la página en vivo..."
         echo -e "Presiona 'Enter' para dejar de ejecutar el servidor..."; read
         echo -e "[*] Totrixxel está cerrando el servidor..."
         service apache2 stop
-        echo -e "[*] El servidor xTx se ha cerrado correctamente."
-    elif [ $qaction == "4" ]
-    then
-        echo -e "[1] Chat    [2] Reverse Shell"; read qncaction
-        if [ $qncaction == "1" ]
-        then
-            echo -e "[*] Creando chat en el puerto: 70450..."
-            netcat -lp 70450
-        elif [ $qncaction == "2" ]
-        then
-            echo -e "[*] Creando Reverse Shell en el puerto 80920..."
-            netcat -lp 80920 -e /bin/bash
-        fi
+        echo -e "[*] El servidor Apache se ha cerrado correctamente."
+        kill $pid
+        echo -e "[*] El servidor Ngrok se ha cerrado correctamente."
     fi
 }
 
